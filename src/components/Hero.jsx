13cd02ef13cdfa8bd802aa8df6ae07f22968b630
jashan-bhayana <jashanbhayana5@gmail.com>
2024-01-09 00:01:58 +0530

    change in computer.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,7 @@ const Hero = () => {
       <section className="relative w-full h-screen mx-auto">
         <div
           className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+           style={{zIndex:1}}
         >
           <div className="flex flex-col justify-center items-center mt-5">
             <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -30,9 +31,10 @@ const Hero = () => {
         <ComputersCanvas />
 
 
-        <div className='absolute xs:bottom-10 bottom-0 w-full flex justify-center items-center'>
+        <div className='absolute xs:bottom-10 bottom-0 w-full flex justify-center items-center' style={{zIndex:1}}>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'
+          style={{zIndex:1}}>
             <motion.div
               animate={{
                 y: [0, 24, 0],
